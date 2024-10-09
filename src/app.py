5d237a383263dcaa81370c699df94c13335d2702
Ada <the.nostra.tymus@gmail.com>
2024-10-08 21:48:37 -0400

    updated to have better color on the datatable, now redirects after input, performs simple input validation.

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, url_for, request, send_file
 
 import pandas as pd
 import json
@@ -10,13 +10,8 @@ import os
 
 app = Flask(__name__)
 
-data = {
-    "Date": ["2024-01-01", "2024-01-02", "2024-01-03", "2024-01-04", "2024-01-05"],
-    "Value": [10, 15, 13, 17, 19],
-    "Unique Value": [101, 102, 103, 104, 105]  # Unique values for each entry
-}
-
-df = pd.DataFrame(data)
+if not os.path.exists("data/"):
+    os.makedirs("data/")
 
 
 def get_entries(add_invalid=False):
@@ -24,9 +19,13 @@ def get_entries(add_invalid=False):
 
     for root, dirs, files in os.walk("data"):
         for file in files:
+            if not file.endswith(".json"):
+                continue
             with open(os.path.join(root, file), 'r') as file_in:
                 temp_dict = json.load(file_in)
                 temp_dict['datetime'] = datetime.fromtimestamp(float(temp_dict['ts']))
+                if temp_dict['date_override'] != "":
+                    temp_dict['datetime'] = datetime.strptime(temp_dict['date_override'], "%m-%d-%Y")
                 if temp_dict['valid']:
                     entries.append(temp_dict)
                 elif add_invalid:
@@ -35,13 +34,26 @@ def get_entries(add_invalid=False):
     return entries
 
 
-def get_dataframe():
-    entries = get_entries()
+def get_dataframe(add_invalid=False, last_90_days_only=True):
+    entries = get_entries(add_invalid)
     df = pd.DataFrame.from_dict(entries)
+    df = df.sort_values(["datetime"])
+    df['amount'] = df['amount'].astype(int)
+    if last_90_days_only:
+        mask = df['datetime'].apply(lambda x: (datetime.now() - x) < timedelta(days=90))
+        df = df[mask]
+
     return df
 
 
-# Route for GET request to display the form using form.html from the templates directory
+@app.route('/dump', methods=['GET'])
+def dump():
+    file_name = os.path.join("data/", "dump.csv")
+    df = get_dataframe(add_invalid=True, last_90_days_only=False)
+    df.to_csv(file_name, index=False)
+    return send_file(file_name, as_attachment=True)
+
+
 @app.route('/', methods=['GET'])
 def entry():
     return render_template('entry.html')
@@ -56,7 +68,9 @@ def generate_table(df):
     """ Generate an HTML table from a DataFrame. """
     # consider adding CSS for the table
     # arrange the df columns to a way that make more sense
-    # transform the date time field
+    df = df.sort_values(['datetime'], ascending=False)
+    df = df.reindex(columns=["datetime", 'whomst', 'tag', 'amount', "ts", 'notes'])
+    df['datetime'] = df['datetime'].apply(lambda x: x.strftime("%m-%d-%Y"))
 
     avoid_columns = ['valid', 'date_override']
 
@@ -64,16 +78,12 @@ def generate_table(df):
     # Create table header
     table_html += '<div class="row">'
     for column in df.columns:
-        if column in avoid_columns:
-            continue
         table_html += f'<div class="col"><b>{column.upper()}</b></div>'
     table_html += '</div>'
 
     for idx in range(df.shape[0]):
         table_html += '<div class="row">'
         for column in df.columns:
-            if column in avoid_columns:
-                continue
             table_html += f'<div class="col"><small>{df[column].iloc[idx]}</small></div>'
         table_html += '</div>'
 
@@ -112,6 +122,18 @@ def submit():
     notes = request.form.get('Notes')
     date_override = request.form.get('Date')
     ts_code = str(time.time())
+
+    try:
+        int(amount)
+    except:
+        return "Invalid Amount Type"
+
+    try:
+        if date_override != "":
+            datetime.strptime(date_override, "%m-%d-%Y")
+    except:
+        return "Invalid Date Override"
+
     with open(f"data/{ts_code}.json", 'w') as file_out:
         json.dump({'whomst': whomst,
                    'tag': tag,
@@ -121,7 +143,7 @@ def submit():
                    'ts': ts_code,
                    'valid': True}, file_out)
 
-    return f"Entry ID: {ts_code}"
+    return redirect(url_for('dashboard'))
 
 
 @app.route('/remove', methods=['POST'])
@@ -140,7 +162,7 @@ def remove():
     with open(file_name, 'w') as file_out:
         json.dump(entry_dict, file_out)
 
-    return "Entry has been removed."
+    return redirect(url_for('dashboard'))
 
 
 if __name__ == '__main__':
